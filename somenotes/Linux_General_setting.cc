7d089434967e07d28fe2d690e21d38f1e5583abc
pentakilj <ljsnaruto@gmail.com>
2020-06-06 15:23:10 +0800

    update command orders

--- somenotes/Linux_General_setting.cc
+++ somenotes/Linux_General_setting.cc
@@ -6,8 +6,37 @@ Manjaro( Arch ):
 
   ## Use Arch linux mirrors to install brand new packages
   ## Manjaro mirrors to upgrade system
-
-  ## systemd默认的时间工具
+  ============== 0. LVM管理home swap ==============
+  ## 使用LVM管理 home 及swap, 先在界面创建（不要裸盘，免得操作失误）
+  ## lsblk
+  ## fdisk /dev/sdb1  or parted -l
+  # PV
+  pvcreate /dev/sdb1
+  pvdisplay
+  pvscan
+  # VG
+  vgcreate VG /dev/sdb1
+  vgdisplay
+  # LV
+  lvcreate -L 112G VG -n lvol_home
+  lvcreate -l 100%FREE VG -n lvol_swap
+  lvdisplay
+  lvdisplay | grep -iE 'path|size'
+  vgchange -ay
+  # 格式化并备份挂载home
+  mkf.ext4 /dev/VG/lvol_home
+  /bin/cp -rfp /home /home.bk
+  mount /dev/VG/lvol_home /home
+  /bin/cp -rfp /home.bk/* /home
+  # 启用swap
+  mkswap /dev/VG/lvol_swap
+  swapon /dev/VG/lvol_swap
+  # 查看UUID， 并在fstab中设置启动挂载
+  blkid /dev/VG/lvol_home
+  blkid /dev/VG/lvol_swap
+  vim /etc/fstab
+  # 查看内存是否有swap
+  ============== 1. systemd默认的时间工具 ==============
   ## 设置 系统时间, windows 使用的localtime, MAC, linux都是UTC(与格林威时间大致相同)
   ## 直接开启ntp同步时间就行， 待确定如何修改同步服务器
   ## 不需要再执行 hwclock --systohc， 也不会和windows时间冲突？？
@@ -22,7 +51,7 @@ Manjaro( Arch ):
   ## python3 has been used , do not link /usr/bin/python to python2
   ## use 'ls -l /usr/bin/python' to check
 
-  ## 自动选出国内源
+  ============== 2. 自动选出国内源 ==============
   sudo pacman-mirrors -i -c China -m rank
   sudo pacman -Syyu
   ## 手动
@@ -35,14 +64,28 @@ Manjaro( Arch ):
   Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
   # then install archlinuxcn-keyring to import GPG key
   sudo pacman -Sy archlinuxcn-keyring
+  ============== 3. 设置历史记录 ==============
+  # 编辑.bashirc
+  echo "export HISTTIMEFORMAT=\"%Y/%m/%d %T \"" >> ~/.bashrc
+  echo "export HISTSIZE=10000" >> ~/.bashrc
+  echo "export HISTFILESIZE=10000" >> ~/.bashrc
+  tail ~/.bashrc
 
-  ## VirtualBox
-  ## make sure BIOS-setting is ok,the kernel should use what 'uname -r' shows
-  ## not use *manjaro* kernel, but a version-clearly kernal
-  sudo pacman -S virtualbox
-  sudo modprobe vboxdrv
-  sudo pacman -Sc
-  ## docker sources
+  ============== 5. 安装软件 ==============
+  ## 1. 不要 pepper-flash flashplugin gedit ，
+  ## 2. virtualbox根据内核单独配置，
+  ## 3. office选still, 增加meld (diff GUI), brige: qv2ray and it's core
+  sudo pacman -S arc-gtk-theme  vlc vim audacity audacious inkscape bleachbit stellarium git nmap screenfetch tree xed docker firefox chromium transmission-gtk thunderbird atom curl redshift fcitx fcitx-googlepinyin fcitx-gtk3 fcitx-configtool bash-completion virtualbox libreoffice-still meld unzip
+  # 可选软件包：tools http-api test, download
+  sudo pacman -S httpie aria2 curl uget
+  # others : gnome map, redshift, net-tools for ifconfig and route, bind-tools(dnsutils) for dig and nslookup
+  # night light, ~/.config/redshift.conf
+  sudo pacman -S redshift python-xdg (for redshift) #Okular(PDF)
+  ----------- a. docker -----------
+  # 增加docker 用户, 用户组
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  ######### docker sources ############
   dnsmasq
         build yourself
   nextcloud
@@ -54,6 +97,41 @@ Manjaro( Arch ):
         forked
   163proxy
         forked
+  ----------- b. Atom -----------
+  ## 需要的插件， 列选择，  中文包
+  Sublime-Style-Column-Selection
+  simplified-chinese-menu
+  ----------- c. fcitx -----------
+  ## 输入法
+  ## sudo pacman -S fcitx fcitx-googlepinyin fcitx-gtk3 fcitx-configtool (fcitx-im)
+  ## add script to ~/.xprofile starts when booting to make variables effective
+  export GTK_IM_MODULE=fcitx
+  export QT_IM_MODULE=fcitx
+  export XMODIFIERS=@im=fcitx
+  ----------- d. Netease-cloud-music -----------
+  ## 安装中文源后
+  ## download official .deb packages from music.163.com is much better
+  ## replace all packs after install via pacman
+  sudo pacman -S netease-cloud-music
+  ----------- d. VirtualBox -----------
+  ## make sure BIOS-setting is ok,the kernel should use what 'uname -r' shows
+  ## not use *manjaro* kernel, but a version-clearly kernal
+  sudo pacman -S virtualbox
+  sudo modprobe vboxdrv
+  sudo pacman -Sc
+
+  ============== 5. 中文配置， 得先安装字体 ==============
+  sudo pacman -S ttf-roboto noto-fonts ttf-dejavu
+  # 文泉驿
+  sudo pacman -S wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei
+  # 思源字体 及 abobe开源
+  sudo pacman -S noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+  ##  mkdir .config/fontconfig
+  # 自动微调 微调 抗锯齿 内嵌点阵字体
+  vim .config/fontconfig/fonts.conf  增加字体配置文件，更好看，不虚化，参加其他配置文件+
+  # 然后调高文本缩放比例 1.3， 微调饱和
+  # 界面字体默认为：Cantarell Regular (10号，文档为11号), 等宽 Source code pro Regular(10号)
+
   ## Manjaro conflict
   sudo rm -rf chrome/chromium/firefox
   #sudo rm  /usr/lib/firefox/distribution/distribution.ini
@@ -70,72 +148,6 @@ Manjaro( Arch ):
   ## sudo rm -rf  /usr/lib/chrome/bookmarks.html
   #sudo pacman -R firefox-i18n-zh-cn firefox-i18n-en-us thunderbird-i18n-en-us thunderbird-i18n-zh-cn npapi-vlc vlc-nightly
   #sudo pacman -R firefox thunderbird transmission-gtk
-
-  # 中文配置， 得先安装字体
-  sudo pacman -S ttf-roboto noto-fonts ttf-dejavu
-  # 文泉驿
-  sudo pacman -S wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei
-  # 思源字体 及 abobe开源
-  sudo pacman -S noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
-  ##  mkdir .config/fontconfig
-  # 自动微调 微调 抗锯齿 内嵌点阵字体
-  vim .config/fontconfig/fonts.conf  增加字体配置文件，更好看，不虚化，参加其他配置文件
-
-  # 主要软件， 不要 pepper-flash flashplugin gedit ， virtualbox根据内核单独配置， office, meld (diff GUI), brige: qv2ray and it's core
-  sudo pacman -S arc-gtk-theme  vlc vim audacity audacious inkscape bleachbit stellarium git nmap screenfetch tree xed docker firefox chromium transmission-gtk thunderbird atom curl redshift fcitx fcitx-googlepinyin fcitx-gtk3 fcitx-configtool bash-completion virtualbox libreoffice-still meld
-  # 增加docker 用户
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-
-  ## Some packages , Sublime-Style-Column-Selection for atom
-  # tools http-api test, download
-  sudo pacman -S httpie aria2 curl uget
-  # others : gnome map, redshift, net-tools for ifconfig and route, bind-tools(dnsutils) for dig and nslookup
-
-  # night light, ~/.config/redshift.conf
-  sudo pacman -S redshift python-xdg (for redshift) #Okular(PDF)
-
-  ## fcitx 单独配置部分参数
-  ## sudo pacman -S fcitx fcitx-googlepinyin fcitx-gtk3 fcitx-configtool (fcitx-im)
-  ## add script to ~/.xprofile starts when booting to make variables effective
-  export GTK_IM_MODULE=fcitx
-  export QT_IM_MODULE=fcitx
-  export XMODIFIERS=@im=fcitx
-
-  ## Netease-cloud-music 安装中文源后，网易云
-  ## download official .deb packages from music.163.com is much better
-  ## replace all packs after install via pacman
-  sudo pacman -S netease-cloud-music
-
-  ## 使用LVM管理 home 及swap, 先在界面创建，免得操作失误
-      ## lsblk
-      ## fdisk /dev/sdb1  or parted -l
-      # PV
-      pvcreate /dev/sdb1
-      pvdisplay
-      pvscan
-      # VG
-      vgcreate VG /dev/sdb1
-      vgdisplay
-      # LV
-      lvcreate -L 112G VG -n lvol_home
-      lvcreate -l 100%FREE VG -n lvol_swap
-      lvdisplay
-      lvdisplay | grep -iE 'path|size'
-      vgchange -ay
-      # 格式化并备份挂载home
-      mkf.ext4 /dev/VG/lvol_home
-      /bin/cp -rfp /home /home.bk
-      mount /dev/VG/lvol_home /home
-      /bin/cp -rfp /home.bk/* /home
-      # 启用swap
-      mkswap /dev/VG/lvol_swap
-      swapon /dev/VG/lvol_swap
-      # 查看UUID， 并在fstab中设置启动挂载
-      blkid /dev/VG/lvol_home
-      blkid /dev/VG/lvol_swap
-      vim /etc/fstab
-      # 查看内存是否有swap
 
   ## Python2.7 && packages
   sudo pacman -S python2-pip
@@ -247,7 +259,6 @@ Fedora26: (install sqlite to completion)
   # for matplotlibsss
   sudo dnf install python2-tkinter
   pip install --user ipython scrapy matplotlib
-
 
   ## netease-cloud-music
   sudo mv data/usr/lib/netease-cloud-music /usr/lib
